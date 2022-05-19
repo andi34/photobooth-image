f22d1470e209a44eb267e3fbb9fb7c89685db38b
Andreas Blaesius <andi@unlegacy-android.org>
2022-05-19 06:59:05 +0200

    image_builder(raspberry): another attempt to disable user creation

--- image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
+++ image_builder/raspberry/stage-photobooth/01-disable-usercreation/00-run.sh
@@ -17,8 +17,20 @@ systemctl stop userconfig
 systemctl disable userconfig
 systemctl mask userconfig
 
+# specific changes for Pi OS with Desktop
 if [ -e /etc/xdg/autostart/piwiz.desktop ]; then
     rm -f /etc/xdg/autostart/piwiz.desktop
 fi
+if [ -e /etc/sudoers.d/010_wiz-nopasswd ]; then
+    rm /etc/sudoers.d/010_wiz-nopasswd
+fi
+if [ -e /etc/xdg/autostart/deluser.desktop ]; then
+    rm /etc/xdg/autostart/deluser.desktop
+fi
+
+if [ getent passwd "rpi-first-boot-wizard" > /dev/null ]; then
+    userdel -r rpi-first-boot-wizard
+fi
+systemctl enable getty@tty1
 EOF
 
